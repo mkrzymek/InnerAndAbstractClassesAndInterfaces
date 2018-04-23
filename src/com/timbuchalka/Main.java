529969dd2a593951101dcae86756b563fc1b9738
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-23 11:31:11 +0200

    inner classes and anonymous classes

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -1,5 +1,7 @@
 package com.timbuchalka;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -96,14 +98,70 @@ public class Main {
     }
 
 
-
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
 
     public static void innerClass() {
 
         Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+//        mcLaren.addGear(1, 5.3);
+//        mcLaren.addGear(2, 10.6);
+//        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClitch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClitch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClitch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClitch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
+
+//        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        System.out.println(first.driveSpeed(1000));
+
+        class ClickListner implements Button.OnClickListener {
+            public ClickListner() {
+                System.out.println("Ive beend attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
+
+//        btnPrint.setOnClickListener(new ClickListner());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+
+
+
+
+    }
+
+    private static void listen() {
+
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 
 
