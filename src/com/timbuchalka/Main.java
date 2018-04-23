a1514c1a93be0a039a58c8b0593ba4a2159748c6
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-23 11:06:56 +0200

    inner calsses part 1

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -97,9 +97,22 @@ public class Main {
 
 
 
+
+
+    public static void innerClass() {
+
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        System.out.println(first.driveSpeed(1000));
+    }
+
+
+
+
     public static void main(String[] args) {
 
 //        interfaces();
-        interfaceChallenge();
+//        interfaceChallenge();
+        innerClass();
     }
 }
